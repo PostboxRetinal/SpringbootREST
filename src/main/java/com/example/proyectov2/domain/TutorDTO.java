550cd3018307a8042209d948612edb8837bc28bb
PostboxRetinal <78338192+PostboxRetinal@users.noreply.github.com>
2024-05-03 21:31:53 -0500

    v0.2 - Model Creation and Controller adoption - Recreated services - Removed DTO

--- src/main/java/com/example/proyectov2/domain/TutorDTO.java
+++
@@ -1,4 +0,0 @@
-package com.example.proyectov2.domain;
-
-public class TutorDTO {
-}
---
+++ src/main/java/com/example/springbootAPI/service/STutor.java
@@ -0,0 +1,13 @@
+package com.example.springbootAPI.service;
+
+import com.example.springbootAPI.model.Tutor;
+import java.util.List;
+import java.util.Optional;
+
+public interface STutor {
+    String crearTutor(Tutor tutor);
+    List<Tutor> listarTutores();
+    Optional<Tutor> tutorPorId(int tutorid);
+    String actualizarTutorPorId(Tutor tutor);
+    List<Tutor> mostrarCalificacionesMayoresAN(double calificacion);
+}
